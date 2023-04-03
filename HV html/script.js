bf58cbf0f5152b2a5974a2b9e680ecf07476a3b0
Kevin Villegas <kevin94villegas22@gmail.com>
2023-04-02 20:25:17 -0500

    HV animaciones y foto

---
+++ HV html/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multipe-text", {
+    strings: ["Tecnologo en Sistemas.","Productor Multimedial."],
+    typeSpeed: 100, 
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
